adac5f8e6d4a6556506ff730bffd61a5b6f84cbc
Your Name <Your Email>
2023-03-02 11:58:40 +0700

    comit

--- keyboards/0_a/keymaps/default/keymap.c
+++ keyboards/0_a/keymaps/default/keymap.c
@@ -37,3 +37,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_LCTL,          KC_LALT,          KC_SPC,  KC_LGUI,                   KC_SPC,           KC_RALT,                            KC_RCTL
     )
 };
+
